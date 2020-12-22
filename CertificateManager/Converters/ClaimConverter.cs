d5b41867943e072a480d1dfcc9c288c13510b90e
freeedy <ismayilzadeh.farid@gmail.com>
2020-12-22 18:15:08 +0400

    Certificate Claims ready . Task #10  CertificateClaims

---
+++ CertificateManager/Converters/ClaimConverter.cs
@@ -0,0 +1,27 @@
+﻿extern alias BC;
+
+using BC::Org.BouncyCastle.Asn1;
+using BC::Org.BouncyCastle.X509;
+using FrdCoreCrypt.Objects;
+using FrdCoreCrypt.Objects.Static;
+using FrdCoreCrypt;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using X509Certificate = BC::Org.BouncyCastle.X509.X509Certificate;
+
+namespace FrdCoreCrypt.Converters
+{
+    public class ClaimConverter
+    {
+        
+
+
+
+
+
+
+    }
+}
---
+++ CertificateManager/Enums/CertificateClaims.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrdCoreCrypt.Enums
+{
+    public class CertificateClaims
+    {
+        public const string SubjectCommonName = "cert_subj_CN";
+        public const string SubjectGivenName = "cert_subj_GN";
+        public const string SubjectSurname = "cert_subj_SN";
+        public const string SubjectCountry = "cert_subj_C";
+        public const string SubjectOrganisation = "cert_subj_O";
+        public const string SubjectOrganisationUnit = "cert_subj_OU";
+        public const string SubjectSerialNUmber = "cert_subj_SERIALNUMBER";
+        public const string SubjectTitle = "cert_subj_T";
+        public const string SubjectStateProvince = "cert_subj_SP";
+        public const string CertificateOCSPStatus = "cert_validation_OCSPStatus";
+        public const string CertificateOCSPResultTime = "cert_validation_OCSPProducedTime";
+        public const string CertificateChainValidation = "cert_validation_Chain";
+        public const string CertificateValidFrom = "cert_VFR";
+        public const string CertificateValidTo = "cert_VTO";
+        public const string CertificateAuthorityKeyIdentifier = "cert_AKI";
+
+    }
+}
