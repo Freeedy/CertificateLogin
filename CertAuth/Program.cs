c8762f3f33d4009e95ba4eb7d9d1dfacf1849706
freeedy <ismayilzadeh.farid@gmail.com>
2021-01-06 12:24:12 +0400

    log4net  configuration

--- CertAuth/Program.cs
+++ CertAuth/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System.IO;
 using System.Net;
+using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
+using log4net;
+using log4net.Config;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.AspNetCore.Server.Kestrel.Https;
@@ -16,6 +19,13 @@ namespace CertAuth
 
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
+         var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+
+
+
             var certificate =GetServiceCertificate("local") ;
             var whb = Host.CreateDefaultBuilder(args)
           .ConfigureWebHostDefaults(webBuilder =>
